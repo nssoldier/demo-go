6d67d6a25cee7b3953d1ac116fa9c9a6def33f67
nssoldier <hoangbv.nde17048@vtc.edu.vn>
2024-01-16 09:04:53 +0700

    update using unbufferd channel

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func crawlData(length int, name string) (n []int) {
 	for count := 0; count < length; count++ {
-		time.Sleep(time.Duration(rand.Intn(2000)) * time.Millisecond)
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
 		var elem = rand.Intn(100)
 		n = append(n, elem)
 		fmt.Println(fmt.Sprintf(
@@ -28,30 +28,64 @@ func crawlData(length int, name string) (n []int) {
 }
 
 func main() {
-	var waitGroup sync.WaitGroup
-	var result []int
+	//var waitGroup sync.WaitGroup
+	//var result []int
+	//var mutex sync.Mutex
+	//
+	//waitGroup.Add(3)
+	//
+	//fmt.Println("dcm start")
+	//for count := 0; count < 3; count++ {
+	//	count := count
+	//	go func() {
+	//		var arrLength = rand.Intn(20)
+	//		var name = fmt.Sprintf("Array %d", count+1)
+	//
+	//		fmt.Println(fmt.Sprintf("Starting add elems to %s with length is %d", name, arrLength))
+	//		var response = crawlData(arrLength, name)
+	//		fmt.Print(fmt.Sprintf("%s done: ", name))
+	//		fmt.Println(response)
+	//		mutex.Lock()
+	//		result = append(result, response...)
+	//		mutex.Unlock()
+	//		waitGroup.Done()
+	//	}()
+	//}
+	//
+	//waitGroup.Wait()
+	//
+	//fmt.Println(result)
+	//fmt.Println("Done!")
 
-	fmt.Println("dcm start")
+	var (
+		waitGroup   sync.WaitGroup
+		threadLimit = 3
+		resultChan  = make(chan []int, threadLimit)
+		res         []int
+	)
+
 	waitGroup.Add(3)
 
-	for count := 0; count < 3; count++ {
+	fmt.Println("dcm start")
+
+	for count := 0; count < threadLimit; count++ {
 		count := count
 		go func() {
-			var arrLength = rand.Intn(20)
+			var arrLength = rand.Intn(8)
 			var name = fmt.Sprintf("Array %d", count+1)
 
 			fmt.Println(fmt.Sprintf("Starting add elems to %s with length is %d", name, arrLength))
 			var response = crawlData(arrLength, name)
 			fmt.Print(fmt.Sprintf("%s done: ", name))
 			fmt.Println(response)
-			result = append(result, response...)
+			resultChan <- response
+			res = append(res, <-resultChan...)
 			waitGroup.Done()
 		}()
 	}
 
 	waitGroup.Wait()
+	close(resultChan)
 
-	fmt.Println(result)
-
-	fmt.Println("Done!")
+	fmt.Println(res)
 }
